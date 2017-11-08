78eb23224a8712d988f222d149b899ea6b1ef761
bac2qh <bac2qh@gmail.com>
2017-11-08 00:06:12 -0600

    speechless

--- e1.py
+++ e1.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 
-r = requests.get("http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2015/office_id:1/stage:General")
+r = requests.get("http://historical.elections.virginia.gov/elections/search/year_from:1924/year_to:2016/office_id:1/stage:General")
 soup = bs(r.content, 'html.parser')
 
 tr = soup.find_all('tr', 'election_item')
